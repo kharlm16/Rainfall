cc58e2032f68ef5510b32175dd9f1575925d9593
Kharl-PC <Kharl-PC@DESKTOP-OSSJBVL>
2024-01-18 07:07:24 +0800

    Add project files.

---
+++ Rainfall.Api/Controllers/RainfallController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rainfall.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RainfallController : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stationId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("id/{stationId}/readings")]
+        public IActionResult GetReadingByStationId(string stationId)
+        {
+
+
+            return Ok(new { Test = stationId });
+        }
+    }
+}
