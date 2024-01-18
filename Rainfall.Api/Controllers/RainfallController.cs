858e5a58996840c362f152e15ae3c07018a058d4
Kharlan Palaganas <kharlm16@outlook.com>
2024-01-18 08:41:13 +0800

    Add swagger definitions

--- Rainfall.Api/Controllers/RainfallController.cs
+++ Rainfall.Api/Controllers/RainfallController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Rainfall.Api.Models.Response;
 
 namespace Rainfall.Api.Controllers
 {
@@ -12,13 +13,22 @@ namespace Rainfall.Api.Controllers
         /// </summary>
         /// <param name="stationId"></param>
         /// <returns></returns>
+        /// <response code="200">A list of rainfall readings successfully retrieved</response>
+        /// <response code="400">Invalid request</response>
+        /// <response code="404">No readings found for the specified stationId</response>
+        /// <response code="500">Internal server error</response>
         [HttpGet]
         [Route("id/{stationId}/readings")]
+        [ProducesResponseType(typeof(RainfallReadingResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Error), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Error), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(Error), StatusCodes.Status500InternalServerError)]
+        [Produces("application/json")]
         public IActionResult GetReadingByStationId(string stationId)
         {
 
 
-            return Ok(new { Test = stationId });
+            return Ok(new RainfallReadingResponse());
         }
     }
 }
--- Rainfall.Api/Program.cs
+++ Rainfall.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "1.0",
+        Title = "Rainfall API",
+        Description = "An API which provides rainfall reading data",
+        Contact = new OpenApiContact
+        {
+            Name = "Sorted",
+            Url = new Uri("https://www.sorted.com")
+        }
+    });
+    options.AddServer(new OpenApiServer
+    {
+        Description = "Rainfall Api",
+        Url = "http://localhost:3000"
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
